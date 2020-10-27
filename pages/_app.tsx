915a3cc99f1e04a0530496b42672e8bb819e57a7
Luzain <haikal.luzain@carsworld.id>
2020-10-27 18:20:18 +0700

    🔨 Edited _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
+import { UserContextProvider } from 'context/UserContext'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { useRouter } from 'next/router'
+import { Fragment, useEffect } from 'react'
 import 'styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const App: React.FC<AppProps> = ({
+  Component,
+  pageProps,
+  router,
+  ...props
+}) => {
+  const { push, pathname } = useRouter()
+
+  useEffect(() => {
+    if (pathname === '/dashboard' && !pageProps.isLogin) {
+      push('/auth/login')
+    }
+  }, [])
+
+  return (
+    <Fragment>
+      <UserContextProvider user={pageProps.user || null}>
+        <Component {...pageProps} />
+      </UserContextProvider>
+    </Fragment>
+  )
 }
 
-export default MyApp
+export default App
